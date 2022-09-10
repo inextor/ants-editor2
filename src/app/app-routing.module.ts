3fb5e663b5ac0a6e4f448506905a812483ba6ad1
nextor <nextor@gmail.com>
2022-09-09 17:14:35 -0700

    First test on routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+		{ path:"",loadChildren: () => import('./view-note/view-note.module').then(m => m.ViewNoteModule) }, 
+		{ path: 'edit-note', loadChildren: () => import('./edit-note/edit-note.module').then(m => m.EditNoteModule) }, 
+		{ path: 'backup', loadChildren: () => import('./backup/backup.module').then(m => m.BackupModule) },
+		{ path: 'list-note', loadChildren: () => import('./list-note/list-note.module').then(m => m.ListNoteModule) 
+	}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
