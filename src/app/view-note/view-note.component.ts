b5616aa4caaecbbb9c94f0d37a826fe4e1bd08c9
Nextor <barrilito@nextor.mx>
2022-09-26 23:20:12 -0700

    adding functionality to view-note

--- src/app/view-note/view-note.component.ts
+++ src/app/view-note/view-note.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { from, mergeMap } from 'rxjs';
+import { Note } from '../classes/Models';
+import { NoteDb } from '../classes/NotesDb';
 
 @Component({
-  selector: 'app-view-note',
-  templateUrl: './view-note.component.html',
-  styleUrls: ['./view-note.component.css']
+	selector: 'app-view-note',
+	templateUrl: './view-note.component.html',
+	styleUrls: ['./view-note.component.css']
 })
-export class ViewNoteComponent implements OnInit {
+export class ViewNoteComponent implements OnInit,OnDestroy{
 
-  constructor() { }
+	note_db:NoteDb | null = null;
+	note: Note| null = null;
+	constructor(public route: ActivatedRoute, public titleService: Title)
+	{
+	}
+	ngOnDestroy(): void {
+		throw new Error('Method not implemented.');
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
 
+		this.route.paramMap.pipe
+		(
+			mergeMap((paramMap:ParamMap)=>
+			{
+				if( this.note_db == null )
+					this.note_db = new NoteDb();
+
+				let key = parseInt( paramMap.get('id') as string ) as number;
+				return from( this.note_db.get( key ) );
+			})
+		)
+		.subscribe((note:Note)=>
+		{
+			this.titleService.setTitle( note.title );
+			this.note = note;
+		});
+	}
 }
